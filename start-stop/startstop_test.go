35e787b6e6b18056de82e70e1bafd009a6b30a16
Björn Ahl <bnal@ba78.me>
2019-05-05 11:47:05 +0200

    test: Adding test for StartStopBuilder function (#2)
    * verify that groupedInstances get generated with correct groupby keys

--- start-stop/startstop_test.go
+++ start-stop/startstop_test.go
@@ -3,7 +3,185 @@ package main
 import (
 	"reflect"
 	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
 )
+
+func TestStartStopBuilder(t *testing.T) {
+	tt := []struct {
+		name  string
+		input ec2.DescribeInstancesOutput
+		event StartStopEvent
+		out   groupedInstances
+	}{
+		{
+			name: "two_groups",
+			input: ec2.DescribeInstancesOutput{
+				Reservations: []*ec2.Reservation{
+					&ec2.Reservation{
+						Instances: []*ec2.Instance{
+							&ec2.Instance{InstanceId: aws.String("i-123456789"),
+								PrivateIpAddress: aws.String("127.0.0.1"),
+								Tags: []*ec2.Tag{
+									&ec2.Tag{Key: aws.String("groupTag"), Value: aws.String("grp1")},
+									&ec2.Tag{Key: aws.String("orderByTag"), Value: aws.String("1")},
+								},
+							},
+							&ec2.Instance{InstanceId: aws.String("i-987654321"),
+								PrivateIpAddress: aws.String("127.0.0.1"),
+								Tags: []*ec2.Tag{
+									&ec2.Tag{Key: aws.String("groupTag"), Value: aws.String("grp1")},
+									&ec2.Tag{Key: aws.String("orderByTag"), Value: aws.String("2")},
+								},
+							},
+							&ec2.Instance{InstanceId: aws.String("i-567891234"),
+								PrivateIpAddress: aws.String("127.0.0.1"),
+								Tags: []*ec2.Tag{
+									&ec2.Tag{Key: aws.String("groupTag"), Value: aws.String("grp2")},
+									&ec2.Tag{Key: aws.String("orderByTag"), Value: aws.String("1")},
+								},
+							},
+						},
+					},
+				},
+				NextToken: aws.String(""),
+			},
+			out: groupedInstances{
+				"grp1": instances{
+					instance{
+						instanceID: "i-123456789",
+						tags: map[string]string{
+							"groupTag":   "grp1",
+							"orderByTag": "1",
+						},
+					},
+					instance{
+						instanceID: "i-987654321",
+						tags: map[string]string{
+							"groupTag":   "grp1",
+							"orderByTag": "2",
+						},
+					},
+				},
+				"grp2": instances{
+					instance{
+						instanceID: "i-567891234",
+						tags: map[string]string{
+							"groupTag":   "grp2",
+							"orderByTag": "1",
+						},
+					},
+				},
+			},
+			event: StartStopEvent{
+				GroupByTag: "groupTag",
+				OrderByTag: "orderByTag",
+				OrderBy: []string{
+					"1",
+					"2",
+				},
+			},
+		},
+		{
+			name: "missing-groupby-tag",
+			input: ec2.DescribeInstancesOutput{
+				Reservations: []*ec2.Reservation{
+					&ec2.Reservation{
+						Instances: []*ec2.Instance{
+							&ec2.Instance{InstanceId: aws.String("i-123456789"),
+								PrivateIpAddress: aws.String("127.0.0.1"),
+								Tags: []*ec2.Tag{
+									&ec2.Tag{Key: aws.String("orderByTag"), Value: aws.String("1")},
+								},
+							},
+						},
+					},
+				},
+				NextToken: aws.String(""),
+			},
+			out: groupedInstances{
+				"<missing_grp_tag>": instances{
+					instance{
+						instanceID: "i-123456789",
+						tags: map[string]string{
+							"orderByTag": "1",
+						},
+					},
+				},
+			},
+			event: StartStopEvent{
+				GroupByTag: "groupTag",
+				OrderByTag: "orderByTag",
+				OrderBy: []string{
+					"1",
+					"2",
+				},
+			},
+		},
+		{
+			name: "one-with-grp-tag-one-missing-grp-tag",
+			input: ec2.DescribeInstancesOutput{
+				Reservations: []*ec2.Reservation{
+					&ec2.Reservation{
+						Instances: []*ec2.Instance{
+							&ec2.Instance{InstanceId: aws.String("i-123456789"),
+								PrivateIpAddress: aws.String("127.0.0.1"),
+								Tags: []*ec2.Tag{
+									&ec2.Tag{Key: aws.String("orderByTag"), Value: aws.String("1")},
+								},
+							},
+							&ec2.Instance{InstanceId: aws.String("i-567891234"),
+								PrivateIpAddress: aws.String("127.0.0.1"),
+								Tags: []*ec2.Tag{
+									&ec2.Tag{Key: aws.String("groupTag"), Value: aws.String("grp2")},
+									&ec2.Tag{Key: aws.String("orderByTag"), Value: aws.String("1")},
+								},
+							},
+						},
+					},
+				},
+				NextToken: aws.String(""),
+			},
+			out: groupedInstances{
+				"<missing_grp_tag>": instances{
+					instance{
+						instanceID: "i-123456789",
+						tags: map[string]string{
+							"orderByTag": "1",
+						},
+					},
+				},
+				"grp2": instances{
+					instance{
+						instanceID: "i-567891234",
+						tags: map[string]string{
+							"orderByTag": "1",
+							"groupTag":   "grp2",
+						},
+					},
+				},
+			},
+			event: StartStopEvent{
+				GroupByTag: "groupTag",
+				OrderByTag: "orderByTag",
+				OrderBy: []string{
+					"1",
+					"2",
+				},
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			gott := startStopBuilder(&tc.input, tc.event)
+			if !reflect.DeepEqual(gott, tc.out) {
+				t.Fatalf("gott: %v, wanted: %v", gott, tc.out)
+			}
+		})
+	}
+}
 
 func TestInstances(t *testing.T) {
 	tt := []struct {
